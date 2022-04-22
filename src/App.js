1be682cc41047c991f97f4de4f0f79e6f735e2bf
huseyinatas <huseyinatas@outlook.com>
2022-04-22 14:23:17 +0300

    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, useAnimation } from 'framer-motion'
+import { useRef, useEffect, useState } from 'react'
 
 function App() {
+  const [width, setWidth] = useState()
+  const carouselRef = useRef()
+  const xUpdate = useRef(0)
+  const animation = useAnimation()
+  useEffect(() => {
+    setWidth(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth)
+
+  }, []);
+  function onLeftClick() {
+    const newXPosition = xUpdate.current + 305;
+
+    animation.start({
+         x: newXPosition > 0 ? 0 : newXPosition,
+    });
+  }
+  function onRightClick() {
+      const newXPosition = xUpdate.current - 305;
+
+      animation.start({
+          x: newXPosition < -2000 ? -2000 : newXPosition,
+      });
+  }
+  function onUpdate(latest) {
+    xUpdate.current = latest.x;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <motion.div className="carousel-container" ref={carouselRef}>
+        <motion.div drag={'x'} onUpdate={onUpdate} initial={false} dragConstraints={{ right: 0, left: -width }} animate={animation} className="carousel">
+          <img src="images/1.png" />
+          <img src="images/2.png" />
+          <img src="images/3.png" />
+          <img src="images/4.png" />
+          <img src="images/5.png" />
+          <img src="images/6.png" />
+        </motion.div>
+      </motion.div>
+      <div className="button-container">
+        <button onClick={() => onLeftClick()} className='prev'>Prev</button>
+        <button onClick={() => onRightClick()} className='next'>Next</button>
+      </div>
     </div>
   );
 }
